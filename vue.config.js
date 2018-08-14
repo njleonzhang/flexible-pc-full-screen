8ac34c0a7d1edfd50e6dc2a3c8d69985bf2ddfe0
njleonzhang <scream3616@sina.com>
2018-08-14 16:55:49 +0800

    demo on github

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const HtmlWebpackInlinePlugin = require('html-webpack-inline-plugin');
 
 module.exports = {
   lintOnSave: false,
+  outputDir: 'docs',
+  baseUrl: 'flexible-pc-full-screen',
   configureWebpack: {
     plugins: [
       new HtmlWebpackInlinePlugin({
